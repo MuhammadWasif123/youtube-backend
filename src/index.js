54718d3d276333780dacc8d06ff20f1b035d9343
Muhammad Wasif Khan <wasifp9@gmail.com>
2025-07-20 01:45:58 +0500

    Mongo DB Connection Tutorial Completed

--- src/index.js
+++ src/index.js
@@ -1,5 +1,45 @@
-import mongoose from "mongoose";
+// require('dotenv').config({path:'./env'})  Not good practice to use this it is common js
 
-function connectDB(){}
+import dotenv from "dotenv"
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path: './env'
+})
 
 connectDB()
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+import mongoose from "mongoose";
+import { DB_NAME } from "./constants";
+
+( async()=>{
+     try{
+       await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+       app.on("error",(error)=>{
+        console.log("Error: ",error);
+        throw error
+       })
+
+       app.listen(process.env.PORT,()=>{
+        console.log(`App is listening on port ${process.env.PORT}`)
+       })
+     } catch(error){
+        console.error("ERROR: ",error)
+        throw err
+     }
+})()
+*/
+
+
