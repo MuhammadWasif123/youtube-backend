c1e79e7d708d736dac6c3325e605b49a5500ef7a
Muhammad Wasif Khan <wasifp9@gmail.com>
2025-07-29 09:41:13 +0500

    Added Youtube utils error handling

--- src/index.js
+++ src/index.js
@@ -2,44 +2,25 @@
 
 import dotenv from "dotenv"
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
     path: './env'
 })
 
 connectDB()
+.then(()=>{
+  app.on("error",(error)=>{
+    console.log("Err: ",error);
+    throw error
+  })
 
-
-
-
-
-
-
-
-
-
-
-
-/*
-import mongoose from "mongoose";
-import { DB_NAME } from "./constants";
-
-( async()=>{
-     try{
-       await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-       app.on("error",(error)=>{
-        console.log("Error: ",error);
-        throw error
-       })
-
-       app.listen(process.env.PORT,()=>{
-        console.log(`App is listening on port ${process.env.PORT}`)
-       })
-     } catch(error){
-        console.error("ERROR: ",error)
-        throw err
-     }
-})()
-*/
+  app.listen(process.env.PORT || 8000,()=>{
+    console.log(`Server is Running at port : ${process.env.PORT}`)
+  })
+})
+.catch((err)=>{
+  console.log("MONGO DB Connection Failed !!!",err)
+})
 
 
