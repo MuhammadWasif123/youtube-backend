0a93ba5280bf897b00beed593e702e388950483e
Muhammad Wasif Khan <wasifp9@gmail.com>
2025-09-20 14:01:38 +0500

    Backend youtube Part One completed along with code

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -6,7 +6,8 @@ import { ApiResponse } from "../utils/ApiResponse.js";
  
 const registerUser = asyncHandler(async (req,res)=> {
     const {fullName,username,email,password} = req.body
-    console.log("email",email);
+    // console.log("email",email);
+    console.log(req.body)
 
     if(
         [fullName,email,username,password].some((field)=>
@@ -16,7 +17,7 @@ const registerUser = asyncHandler(async (req,res)=> {
         throw new ApiError(400,"All fields are required")
     }
 
-    const existedUser = User.findOne({
+    const existedUser = await User.findOne({
         $or: [{ username },{ email }]  
     })
     
@@ -26,7 +27,12 @@ const registerUser = asyncHandler(async (req,res)=> {
 
     console.log("Checking required files for testing", req.files)
     const avatarLocalPath = req.files?.avatar[0]?.path;
-    const coverImageLocalPath = req.files?.coverImage[0]?.path;
+    // const coverImageLocalPath = req.files?.coverImage?.[0]?.path;
+
+    let coverImageLocalPath;
+    if(req.files && Array.isArray(req.files.coverImage && req.files.coverImage.length>0)){
+        coverImageLocalPath =req.files.coverImage[0].path;
+    }
 
     if(!avatarLocalPath){
         throw new ApiError(400,"Avatar file is required")
